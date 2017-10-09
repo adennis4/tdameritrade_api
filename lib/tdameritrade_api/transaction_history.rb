4234bfe48698bac7a97385f2c3180e83f63470af
Andrew Dennis <adennis4@gmail.com>
2017-10-08 19:18:22 -0700

    Add transaction history.

---
+++ lib/tdameritrade_api/transaction_history.rb
@@ -0,0 +1,33 @@
+module TDAmeritradeApi
+  module TransactionHistory
+    HISTORY_URL='https://apis.tdameritrade.com/apps/100/History'
+
+    def get_transaction_history(account_id, options={})
+      request_params = build_transaction_history_params(account_id, options)
+
+      uri = URI.parse HISTORY_URL
+      uri.query = URI.encode_www_form(request_params)
+
+      response = HTTParty.get(uri, headers: {'Cookie' => "JSESSIONID=#{@session_id}"}, timeout: 10)
+      if response.code != 200
+        raise TDAmeritradeApiError, "HTTP response #{response.code}: #{response.body}"
+      end
+
+      bp_hash = {"error"=>"failed"}
+      result_hash = Hash.from_xml(response.body.to_s)
+      if result_hash['amtd']['result'] == 'OK'
+        bp_hash = result_hash['amtd']['history']
+      end
+
+      bp_hash
+    rescue Exception => e
+      raise TDAmeritradeApiError, "error in get_positions() - #{e.message}" if !e.is_ctrl_c_exception?
+    end
+
+    private
+
+    def build_transaction_history_params(account_id, options)
+      {source: @source_id, accountid: account_id}.merge(options)
+    end
+  end
+end
